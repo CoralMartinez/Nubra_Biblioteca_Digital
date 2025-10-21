32c25161628320afbb03d03bd5e45940a041d723
Santyls <123048209@upq.edu.mx>
2025-10-21 15:54:24 -0600

    rutas

--- Biblioteca_Digital/routes/web.php
+++ Biblioteca_Digital/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resources('/home', App\Http\Controllers\HomeController::class);
+Route::resources('/sigup', App\Http\Controllers\SignupController::class);
