000e2d4cdf28fc84cd0ae92754cb3dcf265064c0
CoralMartinez <coralmtzs084@gmail.com>
2025-11-05 08:10:02 -0600

    COMMIT DE REPOSITORIO CRUD
    Líneas de código, vista destinada a mostrar el CRUD del repositorio

---
+++ Biblioteca_Digital/resources/views/auth/repositorio-crud.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>CRUD de Repositorio</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <h1>Gestión del Repositorio</h1>
+        <p>Vista destinada a mostrar el CRUD del repositorio</p>
+        <p>Coral M. Silvestre - Nubra Biblioteca Digital</p>
+        <p>LÍNEA EXTRA PARA HACER EL COMMIT</p>
+
+    </body>
+    </html>
+
+        
